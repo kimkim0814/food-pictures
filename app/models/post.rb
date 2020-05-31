1cbbea3ef11fc3b906e80fcca92d981e8e54d22d
kimkim0814 <tnryj505@gmail.com>
2020-05-31 23:07:28 +0900

    Revert "food-pictures (完成)"
    This reverts commit edbfd70064ccdee8c4b5d01f3f03d9d838197675.

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  validates :image, presence: true
-  belongs_to :user
-  has_many :likes
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'posts#index'
-  resources :posts do
-    resources :likes, only: [:create, :destroy]
-  end
-  resources :likes, only: [:create]
-end
