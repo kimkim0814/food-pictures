d09895acabb24852b698b40bb3237de18bb1774a
kimkim0814 <tnryj505@gmail.com>
2020-06-01 21:18:06 +0900

    gest login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  post '/posts/guest_sign_in', to: 'posts#new_guest'
   root to: 'posts#index'
   resources :posts do
     resources :likes, only: [:create, :destroy]
