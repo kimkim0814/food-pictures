777b8c1204e445f3da9e24dc9feaabe9983b0096
kimkim0814 <tnryj505@gmail.com>
2020-06-11 18:10:37 +0900

    fix name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
   end
 
   private
-  def comments_paramms
+  def comments_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
