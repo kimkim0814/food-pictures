10b282de4a1b204e7b7f363770d951057a48c157
kimkim0814 <tnryj505@gmail.com>
2020-06-16 18:34:08 +0900

    change something

--- db/migrate/20200527134511_create_posts.rb
+++ db/migrate/20200527134511_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :name
-      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_081014) do
+ActiveRecord::Schema.define(version: 2020_06_16_083957) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_06_07_081014) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
