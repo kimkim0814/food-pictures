5d6c1bdb3f715ef63b25bd6796cbcaa6070db98d
kimkim0814 <tnryj505@gmail.com>
2020-06-18 13:53:42 +0900

    Revert "Revert "just push""
    This reverts commit 578e4a38e86f2c8a136912936bb98b70ac54b0cd.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_SECRET_KEY'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key:  ENV['S3_SECRET_KEY'],
       region: 'ap-northeast-1'
     }
