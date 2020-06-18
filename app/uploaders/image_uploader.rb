310c1840a3577954d8bbca5f4f0add9ee13e71e6
kimkim0814 <tnryj505@gmail.com>
2020-06-18 12:57:45 +0900

    fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,17 +18,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],  
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-    end
-  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_SECRET_KEY'],
+      aws_secret_access_key:  ENV['S3_SECRET_KEY'],
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
