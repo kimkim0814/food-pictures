b829ebb3b86c9d18cccb3d1a7a3bca62423ffbcc
kimkim0814 <tnryj505@gmail.com>
2020-06-19 20:51:57 +0900

    コメントアウト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,6 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comments_params)
     redirect_to "/posts/#{comment.post.id}"
-
-    # @comment = Comment.new(comment_params)
-    # if @comment.save
-    #  redirect_to item_path
-    # else
-    #   redirect_to item_path
-    # end
-    # @item = Item.find(params[:item_id])
   end
 
   private
