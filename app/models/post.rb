e75ad503a49672be5c60c6a0114346aabd366b59
kimkim0814 <tnryj505@gmail.com>
2020-07-20 01:10:04 +0900

    create model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :image, presence: true
   belongs_to :user
+  belongs_to :category
   has_many :likes, dependent: :destroy
   has_many :comments
+
 
   def self.search(search)
     return Post.all unless search
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_081014) do
+ActiveRecord::Schema.define(version: 2020_07_18_123402) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_081014) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
     t.integer "user_id"
   end
 
