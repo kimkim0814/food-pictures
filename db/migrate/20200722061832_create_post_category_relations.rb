1a63d79b4e0461188d8442559e71ab8551aaf44f
kimkim0814 <tnryj505@gmail.com>
2020-07-22 15:49:30 +0900

    add column

--- db/migrate/20200722061832_create_post_category_relations.rb
+++ db/migrate/20200722061832_create_post_category_relations.rb
@@ -1,7 +1,8 @@
 class CreatePostCategoryRelations < ActiveRecord::Migration[5.2]
   def change
     create_table :post_category_relations do |t|
-
+      t.integer :category_id
+      t.integer :post_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_123402) do
+ActiveRecord::Schema.define(version: 2020_07_22_061832) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_07_18_123402) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_category_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
