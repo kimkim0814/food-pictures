16c37e2908f305c756ef59c7c9d84f6f8c84305d
kimkim0814 <tnryj505@gmail.com>
2020-07-26 19:00:15 +0900

    validation post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,1 +1,21 @@
 require 'rails_helper'
+
+RSpec.describe Post do
+ 
+  before do 
+    @post = build(:post)
+  end
+
+  describe '投稿機能バリデーション' do
+
+    it '店名が空だと投稿できないこと' do
+      @post.name = ''
+      expect(@post.valid?).to eq(false)
+    end
+
+    it "imageがないと投稿できないこと" do
+      @post.image = ''
+      expect(@post.valid?).to eq(false)
+    end
+  end
+end
