c26314cbed561a73b707f2d8b20664472dafdbcd
kimkim0814 <tnryj505@gmail.com>
2020-07-26 18:48:22 +0900

    make factories

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+
+  factory :post do
+    name              {"寿司屋"}
+    image                 {""}
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
  
   before do 
     @user = build(:user)
