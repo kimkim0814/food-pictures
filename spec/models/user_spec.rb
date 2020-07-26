65a83f7a3d7748a44426638d5155146b31e5501a
kimkim0814 <tnryj505@gmail.com>
2020-07-26 18:21:01 +0900

    test user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,18 @@ RSpec.describe User, type: :model do
       @user.email = ''
       expect(@user.valid?).to eq(false)
     end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it " 重複したemailが存在する場合は登録できないこと " do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
   end
 end
