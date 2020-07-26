ab640a0a121284c65dbc5c131d6c9034498761e4
kimkim0814 <tnryj505@gmail.com>
2020-07-26 18:11:30 +0900

    delete

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,3 @@ module FoodPictures
 end
 
 
-# rspecの生成ファイルの定義
-config.generators do |g|
-  g.test_framework :rspec, 
-        view_specs: false, 
-        helper_specs: false, 
-        controller_specs: false, 
-        routing_specs: false
-end
