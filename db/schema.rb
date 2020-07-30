dded6c1fb954a85ed4829edb72fe06cb9d3f4262
kimkim0814 <tnryj505@gmail.com>
2020-07-31 03:28:57 +0900

    fix-dlete-comment

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_061832) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
